f4f15b60279c26607b26f452cf4d79c05b7e6248
Bill Wilder <codingoutloud@gmail.com>
2024-05-12 06:45:36 -0400

    feat: add ssldays function

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,46 @@ import SmallCard from '../components/SmallCard';
 import { projectIcons } from '../components/Icons';
 
 import { projects } from '../utils/projectsData';
+import React, { useRef } from 'react';
+
+function MyComponent() {
+  const myInputRef = useRef();
+
+  const handleClick = () => {
+    //alert('Das thing: ' + myInputRef.current.value);
+
+    // invoke REST API
+    var url = 'https://gist.githubusercontent.com/codingoutloud/be2edce7c57e8eee771acd4d15dd77e6/raw/ad4b1b9f2259b11f58a6a04c63c1e86bf27efaf2/foo.json'; 
+    url = './HttpTrigger1';
+    // url = './api/HttpTrigger1';
+    // url = './ssldays';
+    // url = './api/ssldays';
+    
+    url = '/HttpTrigger1';
+    url = '/api/HttpTrigger1';
+    url = '/ssldays';
+    url = '/api/ssldays';
+    
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  };
+
+
+  return (
+    <div>
+      <input ref={myInputRef} type="text" />
+      <button onClick={handleClick}>Show Value</button>
+    </div>
+  );
+}
 
 const Home = () => (
   <div className="home">
-    <h1>What Can I Deploy to Static Apps?</h1>
+    <h1>Deployzing to Static Apps!</h1>
+    <p>Choose website to check up on:</p>
+    <MyComponent />
+
     <div className="card-grid">
       {projects.map((project) => {
         const Icon = projectIcons[project.id];
